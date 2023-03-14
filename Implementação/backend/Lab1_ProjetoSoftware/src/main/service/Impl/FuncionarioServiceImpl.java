04c8ce7d607cd5852a1fac891f94d7b288bcf0e4
Leonardo Augusto Pereira do Carmo <78862366+Leozin11@users.noreply.github.com>
2023-03-14 20:52:16 -0300

    Update FuncionarioServiceImpl.java
    Classe implementada

--- Implementação/backend/Lab1_ProjetoSoftware/src/main/service/Impl/FuncionarioServiceImpl.java
+++ Implementação/backend/Lab1_ProjetoSoftware/src/main/service/Impl/FuncionarioServiceImpl.java
@@ -6,13 +6,47 @@ import main.service.FuncionarioService;
 public class FuncionarioServiceImpl implements FuncionarioService {
     @Override
     public Funcionario criarFuncionario(Funcionario funcionario, String tipo) {
-        return null;
+        return new Funcionario(funcionario.getId(), funcionario.getMatricula(), funcionario.getNome(),
+                        funcionario.getNomeFamiliar(), funcionario.getDocumentoLegal(), funcionario.getDepartamento(),
+                        funcionario.getAnoIngresso(), funcionario.getAnoDesligamento(), funcionario.getEndereco(),
+                        funcionario.getSalario());;
     }
 
     @Override
     public Funcionario editarFuncionario(Funcionario funcionario, String tipo) {
-        return null;
+        if (funcionario.getId() != null) {
+            this.setId(funcionario.getId());
+        }
+        if (funcionario.getMatricula() != null) {
+            this.setMatricula(funcionario.getMatricula());
+        }
+        if (funcionario.getNome() != null) {
+            this.setNome(funcionario.getNome());
+        }
+        if (funcionario.getNomeFamiliar() != null) {
+            this.setNomeFamiliar(funcionario.getNomeFamiliar());
+        }
+        if (funcionario.getDocumentoLegal() != null) {
+            this.setDocumentoLegal(funcionario.getDocumentoLegal());
+        }
+        if (funcionario.getDepartamento() != null) {
+            this.setDepartamento(funcionario.getDepartamento());
+        }
+        if (funcionario.getAnoIngresso() != null) {
+            this.setAnoIngresso(funcionario.getAnoIngresso());
+        }
+        if (funcionario.getAnoDesligamento() != null) {
+           this.setAnoDesligamento(funcionario.getAnoDesligamento());
+        }
+        if (funcionario.getEndereco() != null) {
+            this.setEndereco(funcionario.getEndereco());
+        }
+        if (funcionario.getSalario() != null) {
+            this.setSalario(funcionario.getSalario());
+        }
+        return Funcionario();
     }
+    
 
     @Override
     public Funcionario deletarFuncionario(Funcionario funcionario, String tipo) {
